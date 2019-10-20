15d8cd0ab8331718a1ca522998c5c20b17e6043b
unknown <okaforkenechukwu69gkc@gmail.com>
2019-10-20 11:25:04 +0100

    Commented the anthentication code

--- authentication.js
+++ authentication.js
@@ -1,8 +1,10 @@
 function isAuthenticated() {
     const isLoggedIn = localStorage.getItem('user');
+    // checks the availability of a user in local storage
 
     if(!isLoggedIn) {
         window.location.href=("./homepage.html");
+        // if no user is logged on, takes you back to the homepage
     }
 }
 
